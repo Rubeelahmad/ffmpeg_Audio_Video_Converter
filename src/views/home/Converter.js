2b0558695ac3b06c7c1e733e54296f86c0d4e107
Muhammad Afzaal <38922111+AfzaalQader@users.noreply.github.com>
2021-10-28 12:13:12 -0700

    Working on dropzone package for drag and drop images

--- src/views/home/Converter.js
+++ src/views/home/Converter.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-function Converter() {
-    return(
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+    borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
+
+
+function Converter(props) {
+
+    const {
+        getRootProps,
+        getInputProps,
+        open,
+        isDragActive,
+        isDragAccept,
+        isDragReject
+    } = useDropzone({
+        accept: 'image/*', noClick: true,
+        noKeyboard: true
+    });
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isDragActive,
+        isDragReject,
+        isDragAccept
+    ]);
+
+    return (
         <>
-            <div>
-                <h1>Converter Video</h1>
+            <div className="container">
+                <div {...getRootProps({ style })}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+                    <button type="button" onClick={open}>
+                        Open File Dialog
+                    </button>
+                </div>
             </div>
         </>
     )
