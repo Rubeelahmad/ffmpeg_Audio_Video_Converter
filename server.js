fc5999585f93e26930e2848c5e8ddf83ad2fd4f4
Rubeel Ahmed <Rubeelahmad@users.noreply.github.com>
2021-12-02 02:14:31 +0500

    deployment updated

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+//app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+
+});
+console.log("Server runs on port: " , port)
+app.listen(port);
