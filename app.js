4b4f03d713c3ec7414f2fda0be6c6a2f39289bb2
Rubeel Ahmed <Rubeelahmad@users.noreply.github.com>
2021-12-02 23:40:43 +0500

    Upload Updated

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/' , express.static(path.join(__dirname, 'frontend/build')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/api', api)
