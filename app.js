603e535af6c51587f47b6c84b9b0305c02e2ec57
Rubeel Ahmed <Rubeelahmad@users.noreply.github.com>
2021-12-03 01:27:28 +0500

    update after build

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use('/api', api);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/front-end/build/index.html'));
